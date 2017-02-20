7a07e90547afeae14629c808c798fbcaa6a10319
Dhananjay <dhananjaymohnot@gmail.com>
2017-02-21 01:30:16 +0530

    Added header to the API call to fetch list of movies

--- app/src/main/java/com/moviebuff/MainActivity.java
+++ app/src/main/java/com/moviebuff/MainActivity.java
@@ -6,15 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.moviebuff.application.AppApplication;
 import com.moviebuff.constants.AppConstants;
 import com.moviebuff.interfaces.AlertDialog_OnClickInterface;
 import com.moviebuff.utils.AppLog;
 import com.moviebuff.utils.AppUtil;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements AlertDialog_OnClickInterface {
 
@@ -41,12 +47,12 @@ public class MainActivity extends AppCompatActivity implements AlertDialog_OnCli
         // Tag used to cancel the request
         String tag_GET_URL_MOVIES_LIST_req = "string_req";
 
-        StringRequest strReq = new StringRequest(Request.Method.GET,
-                AppConstants.GET_URL_MOVIES_LIST, new Response.Listener<String>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                AppConstants.GET_URL_MOVIES_LIST, null, new Response.Listener<JSONObject>() {
 
             @Override
-            public void onResponse(String response) {
-                AppLog.d(TAG, response);
+            public void onResponse(JSONObject response) {
+                AppLog.d(TAG, response.toString());
                 AppUtil.hideProgressDialog(progressDialog);
                 // TODO parse response and show in view
             }
@@ -60,15 +66,24 @@ public class MainActivity extends AppCompatActivity implements AlertDialog_OnCli
                         + "\n" + error.toString(), MainActivity.this, getString(R.string.Retry), getString(R.string.Exit), TAG, false);
 
             }
-        }); //TODO add header Authorization: Bearer AppConstants.TMDB_API_ACCESS_TOKEN
-        AppLog.d(TAG, "AppConstants.GET_URL_MOVIES_LIST:" + AppConstants.GET_URL_MOVIES_LIST);
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> mapForHeader = new HashMap<>();
+                mapForHeader.put(AppConstants.Authorization_HEADER_KEY, AppConstants.
+                        BEARER_WITH_ACCESS_TOKEN_Authorization_HEADER_VALUE);
+                return mapForHeader;
+            }
+        }; //TODO add header Authorization: Bearer AppConstants.TMDB_API_ACCESS_TOKEN
 
         if (AppUtil.isInternetConnectionAvailable(this)) {
 
             progressDialog = AppUtil.showProgressDialog(this, getString(R.string.Loading), false);
 
+            AppLog.d(TAG, "AppConstants.GET_URL_MOVIES_LIST:" + AppConstants.GET_URL_MOVIES_LIST);
+
             // Adding request to request queue
-            AppApplication.getInstance().addToRequestQueue(strReq, tag_GET_URL_MOVIES_LIST_req);
+            AppApplication.getInstance().addToRequestQueue(jsonObjectRequest, tag_GET_URL_MOVIES_LIST_req);
         } else {
             AppUtil.showAlertDialogWith1Button(this, getString(R.string.PleaseConnectToAWorkingInternetConnection), this, getString(R.string.Retry), TAG, false);
         }
--- app/src/main/java/com/moviebuff/constants/AppConstants.java
+++ app/src/main/java/com/moviebuff/constants/AppConstants.java
@@ -10,4 +10,6 @@ public class AppConstants {
     public static final String TMDB_API_ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYjE4YjFkMGE2NzNiYzdlZmYyZTQ3NDA3MDZlNDY0NCIsInN1YiI6IjU4YTg2N2I1OTI1MTQxNTY2OTAwOTRmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kwV3Z7kXU5y1Uds060EAYj3tC72UI-6lcuSLXaNVscw";
 
     public static final String GET_URL_MOVIES_LIST = TMDB_BASE_URL_LISTING + "list/1";
+    public static String BEARER_WITH_ACCESS_TOKEN_Authorization_HEADER_VALUE = "Bearer " + TMDB_API_ACCESS_TOKEN;
+    public static String Authorization_HEADER_KEY = "Authorization";
 }
